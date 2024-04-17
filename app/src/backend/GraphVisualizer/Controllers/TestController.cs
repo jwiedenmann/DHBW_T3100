c828b76b38822c3360de39fc29147b30efa6e865
Jonas Wiedenmann <jwiedenmann@exone.de>
2024-04-17 21:39:17 +0200

    added test controller

---
+++ app/src/backend/GraphVisualizer/Controllers/TestController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace GraphVisualizer.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class TestController : Controller
+    {
+        public IActionResult Index()
+        {
+            return Ok();
+        }
+    }
+}
