01fd375ccca2a0f10fdf2389a3f385f86723ebd6
Jonas Wiedenmann <jwiedenmann@exone.de>
2024-04-24 08:28:55 +0200

    added env variables

--- app/vite.config.js
+++ app/vite.config.js
@@ -1,9 +1,15 @@
 import { defineConfig } from 'vite';
 import { svelte } from '@sveltejs/vite-plugin-svelte';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 export default defineConfig({
   root: 'src/frontend',
   plugins: [svelte()],
+  define: {
+    'process.env': process.env // This line ensures environment variables are available
+  },
   build: {
     outDir: '../backend/GraphVisualizer/wwwroot',
   },
