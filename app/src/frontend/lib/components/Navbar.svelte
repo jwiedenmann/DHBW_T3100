3ed5deddede8ea0e9120512269c0270b3d8440f4
Jonas Wiedenmann <jwiedenmann@exone.de>
2024-04-29 13:30:46 +0200

    fixed theme toggle

--- app/src/frontend/lib/components/Navbar.svelte
+++ app/src/frontend/lib/components/Navbar.svelte
@@ -1,14 +1,11 @@
 <script>
-  const themes = [
-    "default",
-    "light",
-    "dark",
-    "bumblebee",
-    "emerald",
-    "corporate",
-    "retro",
-    "cyberpunk",
-  ];
+  window.onload = function () {
+    const currentTheme = localStorage.getItem("theme") || "light";
+    console.log(currentTheme);
+    const toggleCheckbox = document.querySelector("[data-toggle-theme]");
+    // @ts-ignore
+    toggleCheckbox.checked = currentTheme !== "light";
+  };
 </script>
 
 <div class="navbar bg-base-100">
@@ -19,23 +16,10 @@
     </a>
   </div>
 
-  <div class="m-5">
-    <h2 class="mb-4 text-2xl text-green-700 font-bold">Toggle</h2>
-    🌞
-    <div class="inline-block w-10">
-      <span data-toggle-theme="synthwave" data-act-class="pl-4" class="border rounded-full border-green-700 flex items-center cursor-pointer w-10 transition-all duration-300 ease-in-out pl-0">
-        <span class="rounded-full w-3 h-3 m-1 bg-green-700">
-        </span>
-      </span>
-    </div>
-    🌚
-  </div>
-
-  <!-- <label class="flex cursor-pointer gap-2">
+  <div class="flex">
     <svg
       xmlns="http://www.w3.org/2000/svg"
-      width="20"
-      height="20"
+      class="w-6"
       viewBox="0 0 24 24"
       fill="none"
       stroke="currentColor"
@@ -46,11 +30,15 @@
         d="M12 1v2M12 21v2M4.2 4.2l1.4 1.4M18.4 18.4l1.4 1.4M1 12h2M21 12h2M4.2 19.8l1.4-1.4M18.4 5.6l1.4-1.4"
       /></svg
     >
-    <input type="checkbox" value="synthwave" class="toggle theme-controller" />
+    <input
+      type="checkbox"
+      class="toggle theme-controller mx-1"
+      data-toggle-theme="synthwave"
+      data-act-class="active"
+    />
     <svg
       xmlns="http://www.w3.org/2000/svg"
-      width="20"
-      height="20"
+      class="w-6"
       viewBox="0 0 24 24"
       fill="none"
       stroke="currentColor"
@@ -59,7 +47,7 @@
       stroke-linejoin="round"
       ><path d="M21 12.79A9 9 0 1 1 11.21 3 7 7 0 0 0 21 12.79z"></path></svg
     >
-  </label> -->
+  </div>
 </div>
 
 <style global lang="postcss">
