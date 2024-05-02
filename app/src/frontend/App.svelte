30f88498b2ae33bca4aa176669106e2b52eb330c
Jonas Wiedenmann <jwiedenmann@exone.de>
2024-05-02 09:31:37 +0200

    added router

--- app/src/frontend/App.svelte
+++ app/src/frontend/App.svelte
@@ -1,21 +1,63 @@
 <script>
   import { onMount } from "svelte";
   import { themeChange } from "theme-change";
-  import Navbar from "./lib/components/Navbar.svelte";
+  import Router, {location, link} from 'svelte-spa-router';
+  import '@fortawesome/fontawesome-free/css/all.min.css'
 
   onMount(() => {
     themeChange(false);
   });
+
+	import Home from './lib/pages/Home.svelte';
+	import Search from './lib/pages/Search.svelte';
+	import Graph from './lib/pages/Graph.svelte';
 </script>
 
 <main>
-  <Navbar />
-  <h1 class="mb-4 text-4xl font-extrabold leading-none">This is a heading!</h1>
+  <!-- <a  href="/">Home</a>
+	<a href="/#/search">Search</a>
+	<a href="/#/graph">Graph</a> -->
+
+  <Router routes={{
+    '/': Home,
+    '/search': Search,
+    '/graph': Graph
+  }} />
+  <!-- <div>
+    <h1
+      class="mb-4 text-4xl font-extrabold leading-none tracking-tight text-gray-900 md:text-5xl lg:text-6xl dark:text-white text-center"
+    >
+      We invest in the world's potential
+    </h1>
+    <p
+      class="mb-6 text-lg font-normal text-gray-500 lg:text-xl sm:px-16 xl:px-48 dark:text-gray-400 text-center"
+    >
+      Here at Flowbite we focus on markets where technology, innovation, and
+      capital can unlock long-term value and drive economic growth.
+    </p>
+    <a
+      href="/"
+      class="inline-flex items-center justify-center px-5 py-3 text-base font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 dark:focus:ring-blue-900"
+    >
+      Learn more
+      <svg
+        class="w-3.5 h-3.5 ms-2 rtl:rotate-180"
+        aria-hidden="true"
+        xmlns="http://www.w3.org/2000/svg"
+        fill="none"
+        viewBox="0 0 14 10"
+      >
+        <path
+          stroke="currentColor"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+          stroke-width="2"
+          d="M1 5h12m0 0L9 1m4 4L9 9"
+        />
+      </svg>
+    </a>
+  </div> -->
 </main>
 
 <style global lang="postcss">
-  main {
-    height: 100rem;
-    background-color: lightgray;
-  }
 </style>
